5dee209bb0ff1722f5be1127f8e644c3961d2f10
Bernhard Zuidema <bernhard.zuidema@slf.ch>
2019-03-28 21:13:33 +0100

    first (noob) version

--- Training/getPics.py
+++ Training/getPics.py
@@ -2,15 +2,19 @@ import numpy as np
 import cv2
 
 
-#Blob Size
-#size = 5
+# no negative samples
+#
+# !ssize.empty() in function 'cv::resize'
+#
 
 
-#picture size pixels
-#pixels = 70
 
-#picture enumeration
-i = 0
+
+
+#set value to not zero
+consistColor = 2
+
+contours = 0
 
 #defining range of color
 boxColorUpper = np.array([255,10,255],np.uint8)
@@ -19,50 +23,53 @@ boxColorLower = np.array([250,0,250],np.uint8)
 #load Video file
 cap = cv2.VideoCapture('Gaimer Training Mirage CT.mp4')
 
+#number of frames
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-ret, frame0 = cap.read()
+_ ,frame = cap.read()
+
 
 for i in range(length-1):
 	#get first frame
 	
-		
-	ret, frame = cap.read()
-	#detect color
-	mask = cv2.inRange(frame, boxColorLower, boxColorUpper)
-	#check if frame0 contains blob
-	contours, _ = cv2.findContours(cv2.inRange(frame0,boxColorLower,boxColorUpper),  cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-	consistColor =  len(contours)
+	#check if 1 video with pink blob other no blob
+	while consistColor != 0 or len(contours) == 0:
+		frame0 = frame
+		_, frame = cap.read()
+		#1 frame read
+		i+=1
 
-	#try:# NB: using _ as the variable name for two of the outputs, as they're not used
-	contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-		#sizeCont = len(contours)
+		#detect color in frame0
+		contours, _ = cv2.findContours(cv2.inRange(frame0,boxColorLower,boxColorUpper), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+		consistColor = len(contours)
+		#detect color in frame
+		contours, _ = cv2.findContours(cv2.inRange(frame, boxColorLower, boxColorUpper), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+		#print(contours)
 	
-	#_,frame2 = cap.read()
-	#contours2,_ = cv2.findContours(cv2.inRange(frame2,boxColorLower, boxColorUpper),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-	if(len(contours)!=0): #check if contours found
+
 		
-		blob = max(contours, key=lambda el: cv2.contourArea(el))
-		M = cv2.moments(blob)
+	blob = max(contours, key=lambda el: cv2.contourArea(el))
+	M = cv2.moments(blob)
 		
-		pixels = int(M["m00"] * 3)
-		
-		#if(M['m00'] > size ) :#color detected
-		#get coordinates of blob
-		if M["m00"] != 0:
+	#division by 0
+	if M["m00"] != 0:
+
+		#size of head picture
+		pixels = (M["m00"])*3
+		#x,y coordinates
+		center = (M["m10"] / M["m00"], M["m01"] / M["m00"])
 			
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			#get previous frame and crop it
-			if consistColor == 0:
-				frame0 = frame0[center[1]-int((pixels/2)):center[1]+int((pixels/2)),center[0]-int((pixels/2)):center[0]+int((pixels/2)) ,:]
-			#else:
-				#ret, frame0 = cap.read()
-				#frame0 = frame0[center[1]-(pixels/2):center[1]+(pixels/2),center[0]-(pixels/2):center[0]+(pixels/2) ,:]
-				#save new frame in folder
-				frame0 = cv2.resize(frame0, dsize=(50,50))
-				cv2.imwrite("frame%d.jpeg" %i, frame0)
-				
-	frame0 = frame
+		#crop image
+		frame0 = frame0[int(center[1]-(pixels/2)):int(center[1]+(pixels/2)),int(center[0]-(pixels/2)):int(center[0]+(pixels/2)) ,:]
+		#check if frame0 isnt 0
+		if len(frame0) > 0 :
+			#scale image
+			frame0 = cv2.resize(frame0, dsize=(50,50))
+			#store frame
+			cv2.imwrite("frame%d.jpeg" %i, frame0)
+
+	#activate while loop
+	consistColor = 3
+
 cap.release()
 cv2.destroyAllWindows()
