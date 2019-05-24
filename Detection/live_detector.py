eb72087303f76eea138cb6a0ecef1e5ec1d4ca57
Indeximal <indeximal@gmail.com>
2019-05-24 19:30:14 +0200

    Added a display for the detections

--- Detection/live_detector.py
+++ Detection/live_detector.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import cv2
 import mss
 import numpy as np
 import tensorflow as tf
@@ -27,16 +28,18 @@ def convert_screen_shot_to_tensor_format(im):
     return np.frombuffer(data, dtype=np.uint8).reshape((1, im_height, im_width, 3))
 
 
-PATH_TO_FROZEN_GRAPH = os.path.join('ssd_mobilenet_v1_coco_2017_11_17',
-    'frozen_inference_graph.pb')
+color_map = [None, (181, 73, 61), (162, 43, 173)] + [(0, 0, 0)] * 100
 
-od_graph_def = tf.GraphDef()
+# Load graph from drive
+PATH_TO_FROZEN_GRAPH = os.path.join('ssd_mobilenet_v1_coco_2018_01_28',
+    'frozen_inference_graph.pb')
+graph_def = tf.GraphDef()
 with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
     serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
+    graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(graph_def, name='')
 
-# Actual detection.
+# Detection Loop
 start_time = time.time()
 with tf.Session() as sess:
 
@@ -60,12 +63,14 @@ with tf.Session() as sess:
     with mss.mss() as sct:
         main_clock = Clock()
         partial_clock = Clock()
-        while True:
-            overhead_time = partial_clock.time_elapsed() * 1000        
+        while True: 
+            # Grab screenshot and convert it
             screen_shot = sct.grab(sct.monitors[1])
             grab_time = partial_clock.time_elapsed() * 1000
+
             image_data = convert_screen_shot_to_tensor_format(screen_shot)
             convert_time = partial_clock.time_elapsed() * 1000
+
             # Run inference
             output_dict = sess.run(tensor_dict,
                                    feed_dict={image_tensor: image_data})
@@ -76,12 +81,29 @@ with tf.Session() as sess:
             boxes = output_dict['detection_boxes'][0]
             scores = output_dict['detection_scores'][0]
 
-            # Detections with a confidence score abouve 80%
+            # Detections with a confidence score above 50%
             valid_detections = [(score, box, classID) for score, box, classID 
-                                in zip(scores, boxes, classes) if score > .8]
-            print(valid_detections)
+                                in zip(scores, boxes, classes) if score > .5]
 
-            fps = 1 / main_clock.time_elapsed()
+            # Display the detection in a window
+            image = np.array(screen_shot)
+            im_w, im_h = screen_shot.size
+            for _, box, classID in valid_detections:
+                y1, x1, y2, x2 = box
+                p1 = (int(x1 * im_w), int(y1 * im_h))
+                p2 = (int(x2 * im_w), int(y2 * im_h))
+                cv2.rectangle(image, p1, p2, color_map[classID], 3)
+            display_image = cv2.resize(image, (960, 540))
+            cv2.imshow("Detections", display_image)
 
-            print("{:.1f} fps ({:.0f}ms|{:.0f}ms|{:.0f}ms|{:.0f}ms)".format(
-                fps, grab_time, convert_time, detect_time, overhead_time))
+            if cv2.waitKey(1) == ord("q"):
+                print("quitting...")
+                break
+
+            # Basic profiling
+            overhead_time = partial_clock.time_elapsed() * 1000 
+            frame_time = main_clock.time_elapsed()
+
+            print("{:.1f} fps ({:.0f}ms: {:.0f}ms|{:.0f}ms|{:.0f}ms|{:.0f}ms)".format(
+                1 / frame_time, frame_time * 1000, grab_time, convert_time, 
+                detect_time, overhead_time))
