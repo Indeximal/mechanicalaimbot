3e34ecb6cbddc2faf12a6a4a517b1caf7bb2c5b0
Indeximal <indeximal@gmail.com>
2019-07-06 13:32:57 +0200

    Added a new data_generator-like module

---
+++ Training/box_extractor.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+
+class ColoredBoxExtractor():
+    def __init__(self, lower_color, upper_color, min_area):
+        self.lower_color = np.array(lower_color, dtype=np.uint8)
+        self.upper_color = np.array(upper_color, dtype=np.uint8)
+        self.min_area = min_area
+
+    def extract(self, frame):
+        # Finds color matching regions
+        mask = cv2.inRange(frame, self.lower_color, self.upper_color)
+        # Finds blobs in the mask
+        # subindex allows for the different return types of OpenCV 3 and 4
+        contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        # Returns bounding boxes of big enough blobs
+        rects = [cv2.boundingRect(c) for c in contours 
+            if cv2.contourArea(c) > self.min_area]
+        return rects
+
+
+class annotated_frames:
+    """This class iterates over the frames of a video and extracts boxes first"""
+    def __init__(self, video_path, extractors):
+        self.video_capture = cv2.VideoCapture(video_path)
+        self.extractors = extractors
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        last_clean_frame = None
+
+        while True:
+            ret, frame = self.video_capture.read()
+            if not ret:
+                raise StopIteration()
+
+            boxes_list = [extractor.extract(frame) for extractor in self.extractors]
+            total = sum([len(boxes) for boxes in boxes_list])
+
+            # If this frame doesn't contain any matches, it is saved for possible later use.
+            if total == 0:
+                last_clean_frame = frame
+
+            # If this is a dirty frame after non-dirty ones, it is returned.
+            elif last_clean_frame is not None:
+                self.keyframe = frame
+                return last_clean_frame, boxes_list, self
+
+    def get_progress(self):
+        curr_frame = videoCapture.get(cv2.CAP_PROP_POS_FRAMES)
+        video_length = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+        return currFrame / video_length
+        
---
+++ Training/extract_viewer.py
@@ -0,0 +1,35 @@
+import argparse
+
+import cv2
+
+import box_extractor
+
+
+parser = argparse.ArgumentParser(description="Shows extractions.")
+parser.add_argument("video_path")
+parser.add_argument("-e", "--extractor", type=int, action="append", nargs=7,
+    metavar=("R1", "G1", "B1", "R2", "G2", "B2", "MinArea"), 
+    help="consumes 5 itegers: R1, G1, B1 (lower), R2, G2, B2 (upper), MinArea")
+parser.add_argument("--line_thinkness", type=int, default=2)
+parser.add_argument("--show_keyframe", action="store_true")
+
+args = parser.parse_args()
+
+
+extractors = [box_extractor.ColoredBoxExtractor((r1, g1, b1), (r2, g2, b2), a)
+    for r1, g1, b1, r2, g2, b2, a in args.extractor]
+
+for frame, boxes_list, obj in box_extractor.annotated_frames(args.video_path, extractors):
+    img = obj.keyframe if args.show_keyframe else frame
+    for boxes, extractor in zip(boxes_list, extractors):
+        color = [int(x) for x in extractor.upper_color]
+        if args.show_keyframe:
+            color = (255, 255, 255)
+        for x, y, w, h in boxes:
+            cv2.rectangle(img, (x, y), (x + w, y + h), 
+               color, args.line_thinkness)
+
+    cv2.imshow("Frame", img)
+    key_pressed = cv2.waitKey(0)
+    if key_pressed == ord("q") or key_pressed == 27:
+        break
