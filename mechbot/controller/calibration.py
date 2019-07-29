20c1b002f6d736dd57ab1734213972d92239c3a6
Indeximal <indeximal@gmail.com>
2019-07-29 13:35:50 +0200

    implemented calibration point gathering for motor 1

--- mechbot/controller/calibration.py
+++ mechbot/controller/calibration.py
@@ -21,9 +21,9 @@ class MovementStateEnum:
 class MoveController:
     def __init__(self, interface):
         # Settings
-        self.motion_threshold = .05
-        self.motion_path = deque(maxlen=3)
-        self.wait_duration = 1.
+        self.motion_threshold = .05  # units
+        self.motion_path = deque(maxlen=20)  # ticks
+        self.wait_duration = .7  # seconds
 
         self.interface = interface
         self.move_state = MovementStateEnum.Idle
@@ -40,8 +40,10 @@ class MoveController:
         self.move_state = MovementStateEnum.Waiting
 
     def _finish(self):
-        self.callback(self.steps, pos, time.time() - self.initial_time)
+        dt = time.time() - self.initial_time
         self.move_state = MovementStateEnum.Idle
+        # Callback might invoke another move_to, so state must be idle before
+        self.callback(self.steps, self.interface.get_input(), dt)
 
     def tick(self):
         # Don't do anything while in idle
@@ -70,28 +72,56 @@ class MoveController:
 
 
 class CalibrationStateEnum:
-    M1_prepare = 1
-    M1_gather = 2
+    M1_Prepare = 1
+    M1_Gather = 2
+    M2_Prepare = 3
 
 
 class CalibrationHelper(MoveController):
-    def __init__(self, interface, wait_duration=70,
-                 max_deflection=.2):
-        super(CalibrationHelper, self).__init__(interface)
-
-        # self.device = initial_device
-        self.step_1 = None
-        self.step_2 = None
-        self.phase = "m1_pos"
-        self.subphase = 0
-        self.done = False
-        self.next_action = time.time()
+    """This class takes controll of an interface to approximate the device"""
+    def __init__(self, interface):
+        # Init
+        MoveController.__init__(self, interface)
         self.m1_points = []
-        self.last_pos = interface.get_input()
-        self.motion_threshold = .05
+        # Settings
+        self.max_deflection = .3
 
-        self.wait_duration = wait_duration
-        self.max_deflection = max_deflection
+    def _get_deflection(self):
+        return vec_len(self.interface.get_input())
+
+    def _act(self):
+        def reaction(steps, pos, t):
+            over_deflection = vec_len(pos) > self.max_deflection
+
+            if self.state == CalibrationStateEnum.M1_Prepare:
+                if over_deflection:
+                    self.state = CalibrationStateEnum.M1_Gather
+
+            if self.state == CalibrationStateEnum.M1_Gather:
+                self.m1_points.append((steps[0], pos))
+                if self.step_1 < 0 and over_deflection:
+                    self.state = CalibrationStateEnum.M2_Prepare
+
+            self._act()
+
+        if self.state == CalibrationStateEnum.M1_Prepare:
+            self.step_1 += 1
+            self.move_to(self.step_1, 0, reaction)
+
+        if self.state == CalibrationStateEnum.M1_Gather:
+            self.step_1 -= 1
+            self.move_to(self.step_1, 0, reaction)
+
+    def start(self):
+        self.state = CalibrationStateEnum.M1_Prepare
+        self.step_1 = 0
+        self.step_2 = 0
+        self._act()
+
+
+class GradientDescentHelper:
+    def __init__(self, device):
+        self.device = device
         self.examples = []
 
     def add_example(self, step1, step2, pos):
@@ -156,41 +186,6 @@ class CalibrationHelper(MoveController):
 
         return [dL_x1 / dx, dL_y1 / dx, dL_x2 / dx, dL_y2 / dx,
                 dL_phi1 / dphi, dL_phi2 / dphi, dL_gap / dgap]
-
-    def _act_old(self):
-        input_pos = self.interface.get_input()
-        deflection = vec_len(input_pos)
-
-        if self.phase == "m1_pos":
-            if self.step_1 is not None:
-                self.m1_points.append((self.step_1, input_pos))
-                self.step_1 += 1
-            else:
-                self.step_1 = 0
-
-            if deflection < self.max_deflection:
-                self.interface.cmd_goto(self.step_1, 0)
-
-            else:
-                self.interface.cmd_goto(0, 0)
-                self.phase == "m1_neg"
-        elif self.phase == "m1_neg":
-            if self.step_1 is not None:
-                self.m1_points.append((self.step_1, input_pos))
-                self.step_1 += 1
-            else:
-                self.step_1 = 0
-
-            if deflection < self.max_deflection:
-                self.interface.cmd_goto(self.step_1, 0)
-            else:
-                self.phase == "m1_neg"
-        else:
-            self.done = True
-
-
-    def is_done(self):
-        return self.done
 
     def get_best_guess(self):
         return self.device
--- mechbot/controller/controller_sim.py
+++ mechbot/controller/controller_sim.py
@@ -36,13 +36,13 @@ simulation = MechanicalSimulator(
 mech_controller = VirtualDevice(motor1, motor2, gap=.2, stick=.1)
 target = (0, 0)
 
-initial_motor_1 = StepperMotor((1.8, 2.1), 200, 1, -2.51)
-initial_motor_2 = StepperMotor((-2.2, 1.94), 200, 1, -0.65)
-initial_device = VirtualDevice(initial_motor_1, initial_motor_2, .14, 0.)
+# initial_motor_1 = StepperMotor((1.8, 2.1), 200, 1, -2.51)
+# initial_motor_2 = StepperMotor((-2.2, 1.94), 200, 1, -0.65)
+# initial_device = VirtualDevice(initial_motor_1, initial_motor_2, .14, 0.)
 
-calibrator = CalibrationHelper(simulation.get_interface(), initial_device)
+calibrator = CalibrationHelper(simulation.get_interface())
 
-calibration_points = []
+# calibration_points = []
 
 tick_counter = 0
 
@@ -51,20 +51,20 @@ force_1_queue = deque(maxlen=30)
 force_2_queue = deque(maxlen=30)
 
 
-def record_calibration_point():
-    point = (simulation.get_input(), motor1.step, motor2.step)
-    calibration_points.append(point)
-    calibrator.add_example(motor1.step, motor2.step, simulation.get_input())
+# def record_calibration_point():
+#     point = (simulation.get_input(), motor1.step, motor2.step)
+#     calibration_points.append(point)
+#     calibrator.add_example(motor1.step, motor2.step, simulation.get_input())
 
 
-def calibrate():
-    epsilon = 0.1
-    for epoch in range(50):
-        calibrator.gradient_descent_step(epsilon)
-        loss = calibrator.calc_loss()
-        if loss < .1:
-            epsilon = 0.01
-        print(epoch, loss, initial_motor_1.pos, initial_motor_2.pos)
+# def calibrate():
+#     epsilon = 0.1
+#     for epoch in range(50):
+#         calibrator.gradient_descent_step(epsilon)
+#         loss = calibrator.calc_loss()
+#         if loss < .1:
+#             epsilon = 0.01
+#         print(epoch, loss, initial_motor_1.pos, initial_motor_2.pos)
 
 
 #  MAIN LOOP
@@ -75,23 +75,23 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                motor1.left()
-                simulation.move = False
-            if event.key == pygame.K_RIGHT:
                 motor1.right()
                 simulation.move = False
-            if event.key == pygame.K_UP:
-                motor2.left()
+            if event.key == pygame.K_RIGHT:
+                motor1.left()
                 simulation.move = False
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_UP:
                 motor2.right()
                 simulation.move = False
-            if event.key == pygame.K_p:
-                record_calibration_point()
+            if event.key == pygame.K_DOWN:
+                motor2.left()
+                simulation.move = False
+            # if event.key == pygame.K_p:
+            #     record_calibration_point()
             if event.key == pygame.K_c:
-                calibrate()
-            if event.key == pygame.K_o:
-                print(calibration_points)
+                calibrator.start()
+            # if event.key == pygame.K_o:
+            #     print(calibration_points)
             if event.key == pygame.K_ESCAPE:
                 running = False
         # Move target
@@ -102,6 +102,10 @@ while running:
 
     # CALCULATIONS
     simulation.tick(0.01)
+    calibrator.tick()
+    if tick_counter % 5 == 0:
+        simulation.loop()
+
     tick_counter += 1
 
     path.append(camera.pixel(simulation.get_input()))
@@ -111,32 +115,33 @@ while running:
     debug.print("1: {:.2f}".format(sum(force_1_queue) / len(force_1_queue)))
     debug.print("2: {:.2f}".format(sum(force_2_queue) / len(force_2_queue)))
 
-    if tick_counter % 5 == 0:
-        simulation.loop()
-
-    #calibrator.tick(tick_counter)
-
     calculated_pos = mech_controller.calculate_cords(motor1.step, motor2.step)
 
     # DRAWING
+    # clear
     screen.fill((255, 255, 255))
 
+    # path
     if len(path) > 1:
         pygame.draw.lines(screen, (50, 50, 50), False, path)
 
-    for pos, _, _ in calibration_points:
+    for _, pos in calibrator.m1_points:
         pygame.draw.circle(screen, (0, 0, 0), camera.pixel(pos), 4, 2)
 
+    # Mechanics
     simulation.draw(screen, camera)
+
+    # Target and guess
     pygame.draw.circle(screen, (110, 110, 200), camera.pixel(
         target), camera.pixel_len(.1), 2)
-
     pygame.draw.circle(screen, (200, 110, 110), camera.pixel(
         calculated_pos), camera.pixel_len(.11), 2)
 
+    debug.print(clock.tick(60))
+
+    # Text
     debug.draw(screen)
 
     pygame.display.flip()
-    clock.tick(250)
 
 pygame.quit()
