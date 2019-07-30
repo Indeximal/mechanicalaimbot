ed83fa6bee43406fe8e74f2b64270f0f2aad44fb
Indeximal <indeximal@gmail.com>
2019-07-30 13:32:24 +0200

    renamed some files

--- pythonpath_snippet.py
+++ pythonpath_snippet.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+# from https://docs.python-guide.org/writing/structure/, 30.07.2019
+
 # Absolute path of the folder this file is in.
 module_path = os.path.abspath(os.path.dirname(__file__))
 # Adds that folder to the python search path.
