d86ba7411360cdf9582001f533ec5ddbb9e32db1
Indeximal <indeximal@gmail.com>
2019-08-23 07:48:50 +0200

    added per image evaluation with the object detection api, but doesn't work

--- mechbot/training/eval_frames.py
+++ mechbot/training/eval_frames.py
@@ -9,6 +9,8 @@ from object_detection.utils import per_image_evaluation
 from mechbot.inference.detection import ObjectDetector
 
 
+class_names = ["head", "body"]
+
 parser = argparse.ArgumentParser(description="Evaluates frames.")
 parser.add_argument("frame_folder")
 parser.add_argument("frozen_graph")
@@ -38,11 +40,11 @@ for filename in label_files:
         # Heads
         for box in label[0] + label[2]:
             boxes.append(box)
-            classes.append(2)  # id for head
+            classes.append(1)  # id for head
         # Bodies
         for box in label[1] + label[3]:
             boxes.append(box)
-            classes.append(1)  # id for body
+            classes.append(2)  # id for body
 
         ground_truth_boxes_list.append(np.array(boxes))
         ground_truth_classes_list.append(np.array(classes))
@@ -59,6 +61,30 @@ with ObjectDetector(args.frozen_graph) as detector:
     for i in range(0, len(frames), batch_size):
         batch = frames[i: i + batch_size]
         boxes, scores, classes = detector.load_and_run_images(batch)
-        predicted_boxes.append(boxes)
-        predicted_classes.append(classes)
-        conficence_scores.append(scores)
+        predicted_boxes.extend(boxes)
+        # predicted_classes.extend(np.expand_dims(classes, 2))
+        # conficence_scores.extend(np.expand_dims(scores, 2))
+        predicted_classes.extend(classes)
+        conficence_scores.extend(scores)
+
+# print("f:", frames)
+# print("gt b:", ground_truth_boxes_list, "gt c:", ground_truth_classes_list)
+# print("b:", predicted_boxes, "c:", predicted_classes, "s", conficence_scores)
+
+evaluator = per_image_evaluation.PerImageEvaluation(2)  # 2 classes
+
+for i in range(len(frames)):
+    results = evaluator.compute_object_detection_metrics(
+        predicted_boxes[i],
+        conficence_scores[i],
+        predicted_classes[i],
+        ground_truth_boxes_list[i],
+        ground_truth_classes_list[i],
+        np.zeros(len(ground_truth_classes_list[i]), dtype=bool),
+        np.zeros(len(ground_truth_classes_list[i]), dtype=bool)
+    )
+    scores, tp_fp_labels, _ = results
+
+    for class_id, score, labels in zip(range(1, len(class_names) + 1), scores, tp_fp_labels):
+        num_gt = sum([1 for c in ground_truth_classes_list[i] if c == class_id])
+        print(class_id, metrics.compute_precision_recall(score, labels, num_gt))
--- mechbot/training/frame_extractor.py
+++ mechbot/training/frame_extractor.py
@@ -9,6 +9,7 @@ from box_extractor import annotated_frames, ColoredBoxExtractor
 parser = argparse.ArgumentParser(description="Extracts frames from a video.")
 parser.add_argument("video_path")
 parser.add_argument("output_folder")
+parser.add_argument("--csv_out", default=None)
 
 args = parser.parse_args()
 
@@ -19,10 +20,14 @@ extractors = [
     ColoredBoxExtractor((0, 200, 200), (50, 255, 255), 500),  # CT Head
     ColoredBoxExtractor((0, 200, 0), (50, 255, 50), 500)  # CT Body
 ]
+extractor_names = ["th", "tb", "cth", "ctb"]
 
+
+csv_rows = [["filename", "width", "height", "class", "xmin", "ymin", "xmax", "ymax"]]
 counter = 0
 for frame, boxes_list, info in annotated_frames(args.video_path, extractors):
     frame_id = "{:05d}".format(counter)
+    frame_file = frame_id + ".png"
     output_path = os.path.join(args.output_folder, frame_id)
 
     width, height, _ = frame.shape
@@ -34,8 +39,20 @@ for frame, boxes_list, info in annotated_frames(args.video_path, extractors):
     with open(output_path + ".txt", "w") as file:
         file.write(repr(rel_y0x0y1x1_boxes))
 
+    for c_name, boxes in zip(extractor_names, boxes_list):
+        for x, y, w, h in boxes:
+            # filename,width,height,class,xmin,ymin,xmax,ymax
+            csv_rows.append([frame_file, width, height, c_name, x, y, x + w, y + h])
+
+
     print("\r{:>3.0f}%".format(info.get_progress() * 100), end="")
 
     counter += 1
 
 print()  # New line
+
+if args.csv_out is not None:
+    import csv
+    with open(args.csv_out, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(csv_rows)
