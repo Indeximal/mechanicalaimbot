fixed inverted x and y. changed camera constant calculation. added a team border.

mechbot/app/gui_thread.py
@@ -11,7 +11,7 @@ from mechbot.utils.fields import CsgoTeamEnum
 
 
 class GUIThread(threading.Thread):
-    """Thread deticated to running the pygame display"""
+    """Thread dedicated to running the pygame display"""
     def __init__(self, run_until, config):
         super(GUIThread, self).__init__(name="GUIThread")
         self.team_selection_listeners = []
@@ -21,6 +21,7 @@ class GUIThread(threading.Thread):
         self.detection_queue = queue.Queue(maxsize=5)
         self.display_device = None
         self.target_pos = None
+        self.joystick_pos = np.zeros(2)
         self.team = CsgoTeamEnum(self.config.default_team)
         self.device_status = "Initializing"
 
@@ -47,7 +48,7 @@ class GUIThread(threading.Thread):
             cam_scale = self.config.device_size / 2.0 / x_limit * width
             x = width - x_limit * cam_scale
             y = y_limit * cam_scale
-            return pygame_utils.Camera(cam_scale, x, y, True)
+            return pygame_utils.Camera(cam_scale, x, y, False)
 
         device_camera = create_camera()
 
@@ -58,7 +59,7 @@ class GUIThread(threading.Thread):
 
         while self.active():
             for event in pygame.event.get():
-                if event.type == pygame.QUIT: 
+                if event.type == pygame.QUIT:
                     for listener in self.shutdown_listeners:
                         listener()
                 # Key event
@@ -80,7 +81,7 @@ class GUIThread(threading.Thread):
                 # Resize
                 if event.type == pygame.VIDEORESIZE:
                     screen_size = width, height = event.w, event.h
-                    screen = pygame.display.set_mode(screen_size, 
+                    screen = pygame.display.set_mode(screen_size,
                                                      pygame.RESIZABLE)
                     device_camera = create_camera()
 
@@ -98,8 +99,8 @@ class GUIThread(threading.Thread):
                     color = self.config.rect_color_per_class[class_id-1]
                     color = tuple(color)
                     rect = (y_min * w, x_min * h,
-                           (y_max - y_min) * w, (x_max - x_min) * h)
-                    pygame.draw.rect(frame_surface, color, rect, 
+                            (y_max - y_min) * w, (x_max - x_min) * h)
+                    pygame.draw.rect(frame_surface, color, rect,
                                      self.config.rect_width)
 
                 scale = min(width / h, height / w)  # rotation swaps h and w
@@ -113,8 +114,8 @@ class GUIThread(threading.Thread):
                 for name, (t_min, t_avg, t_max) in deltas.items():
                     info_display.print("{}: {:.0f}ms>{:.0f}ms>{:.0f}ms".format(
                         name, t_min * 1000, t_avg * 1000, t_max * 1000))
-            info_display.print("Device status: "
-                               + self.device_status.capitalize())
+            # info_display.print("Device status: "
+            #                    + self.device_status.capitalize())
 
             info_display.print("Your Team: " + self.team.name.capitalize())
             info_display.print("Press C or T to change team")
@@ -125,6 +126,11 @@ class GUIThread(threading.Thread):
             if display_surface is not None:
                 screen.blit(display_surface, (0, 0))
 
+            team_color_index = 0 if self.team == CsgoTeamEnum.TERRORISTS else 2
+            team_color = self.config.rect_color_per_class[team_color_index]
+            pygame.draw.rect(screen, tuple(team_color),
+                             (0, 0, width, height), 8)
+
             info_display.draw(screen)
 
             # Draw device
@@ -132,12 +138,18 @@ class GUIThread(threading.Thread):
                 # TODO: Background
                 self.display_device.draw(screen, device_camera)
 
+            # Draw Joystick
+            pygame.draw.circle(screen, (33, 34, 41),
+                               device_camera.pixel(self.joystick_pos),
+                               device_camera.pixel_len(
+                                   self.config.joystick_radius * 1.1), 0)
+
+            # Draw target
             if self.target_pos is not None:
-                # TODO: Draw stick
                 pygame.draw.circle(screen, (110, 110, 200),
                                    device_camera.pixel(self.target_pos),
                                    device_camera.pixel_len(
-                                       self.config.joystick_radius), 2)
+                                       self.config.joystick_radius), 3)
 
             clock.tick(self.config.display_fps)
             pygame.display.flip()
@@ -157,8 +169,9 @@ class GUIThread(threading.Thread):
             self.display_device = device
             self.device_status = "Calibrated"
         elif status_type == DeviceStatusEnum.TARGET:
-            pos, s2, s2 = args
+            pos, s2, s2, current = args
             self.target_pos = pos
+            self.joystick_pos = current
         else:
             self.device_status = status_type.name
 

mechbot/app/motion_thread.py
@@ -57,6 +57,7 @@ class MotionThread(threading.Thread):
             last_target = None
             last_motion = None
             last_joystick = np.zeros(2)
+            pre_last_joystick = np.zeros(2)
             camera_constant = self.config.initial_camera_constant
 
             # Loop: Wait for next detection, process, repeat
@@ -71,11 +72,13 @@ class MotionThread(threading.Thread):
                 targets = self.process_detections(detections)
                 joystick_input = joystick_thread.get_and_reset_total()
                 camera_motion = - joystick_input * camera_constant  # [pxl]
+                camera_motion *= self.config.camera_shift_influence
 
                 # No target to aim at
                 if not targets:
                     last_target = None
                     last_motion = None
+                    pre_last_joystick = last_joystick
                     last_joystick = joystick_input
                     self.move(interface, np.zeros(2))
                     continue
@@ -102,21 +105,17 @@ class MotionThread(threading.Thread):
                     target_motion = last_target - target
                     velocity = target_motion - last_joystick * camera_constant
                     needed_input = (1 / camera_constant
-                                    * (target + camera_motion + velocity))
-
-                    # camera =
-                    #
-                    # last_camera = target_motion - last_joystick * camera_constant
-                    # sensitivity_2d = camera / last_joystick
+                                    * (target_now + velocity))
 
                     if last_motion is not None:
                         # weird calculation to get the camera constant
                         # probably not good
+                        delta_i = last_joystick - pre_last_joystick
                         delta_m = target_motion - last_motion
-                        if vector_utils.vec_len(delta_m) != 0.0:
-                            delta_i = joystick_input - last_joystick
+                        if vector_utils.vec_len(delta_i) != 0.0:
                             new_cam_const = (np.dot(delta_m, delta_i)
-                                             / vector_utils.vec_len(delta_m))
+                                             / (vector_utils.vec_len(delta_i)
+                                             ** 2))
                             logging.debug("k=%s", new_cam_const)
                             # Update cam constant based on a weighted average
                             alpha = self.config.new_camera_constant_weight
@@ -135,8 +134,9 @@ class MotionThread(threading.Thread):
                                             interface.get_input())
                 self.move(interface, pos)
 
-                last_target = target
+                pre_last_joystick = last_joystick
                 last_joystick = joystick_input
+                last_target = target
 
             interface.cmd_goto(0, 0)
 
@@ -155,8 +155,13 @@ class MotionThread(threading.Thread):
             body_id = self.config.t_body_id
 
         heads = [box for box, classID in detections if classID == head_id]
-        # TODO: use body information
         bodies = [box for box, classID in detections if classID == body_id]
+
+        a0 = self.config.body_target_height
+        a1 = 1 - a0
+        body_centers = [np.array([(x_min + x_max) / 2,
+                                  y_min * a0 + y_max * a1]) for
+                        (y_min, x_min, y_max, x_max) in bodies]
 
         # range [-0.5, 0.5]
         centers = [np.array([(x_min + x_max), (y_min + y_max)]) / 2. - 0.5 for
@@ -166,7 +171,7 @@ class MotionThread(threading.Thread):
                                  self.config.monitor_height])
 
         # range [-width/2, width/2] and [-height/2, height/2]
-        offsets = [pos * monitor_size for pos in centers]
+        offsets = [pos * monitor_size for pos in centers + body_centers]
 
         return offsets
 
@@ -181,6 +186,10 @@ class MotionThread(threading.Thread):
         for needed_mov, current_defl in zip(needed_motion, current_pos):
             current_speed = sensitivity_func(current_defl)
             needed_speed = needed_mov / timespan
+
+            if abs(needed_speed) < self.config.movement_threshold:
+                pos.append(0.0)
+                continue
 
             # # TODO: not based on speed but deflection
             # movement_distance = abs(current_speed - needed_speed)
@@ -216,7 +225,8 @@ class MotionThread(threading.Thread):
             s2 = 0
         else:
             s1, s2 = self.device.calculate_steps(pos)
-        self.send_status(DeviceStatusEnum.TARGET, pos, s1, s2)
+        current_pos = interface.get_input()
+        self.send_status(DeviceStatusEnum.TARGET, pos, s1, s2, current_pos)
 
         interface.cmd_goto(s1, s2)
 